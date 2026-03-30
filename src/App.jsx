24f183a46371b85b4436cbc78fe36e7bc3b1b2f5
Zakaria-info <info.zakariahossain01@gnail.com>
2026-03-30 14:21:19 +0600

    Set Up React Project and added Tailwind and DaisiUI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <h2 className='text-3xl text-red-700 font-bold text-center'>Hi, About to start a project</h2>
+    </>
+  )
+}
+
+export default App
